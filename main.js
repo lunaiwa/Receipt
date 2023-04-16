9d91b4df328ea466fb30019f95f847edafcd74f5
lunaiwa <lunaiwazaki@gmail.com>
2023-04-16 00:06:17 -0700

    new

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ window.addEventListener('load', () => {
 	const input = document.querySelector("#new-name");
     //const input = document.querySelector("#new-diet");
     //const input = document.querySelector("#new-preference");
-    //const input = document.querySelector("#new-restaurant");
 	const list_el = document.querySelector("#tasks");
     
 	form.addEventListener('submit', (e) => {
